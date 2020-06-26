17040e328788f012c86cad2de2f69b09f4a39ced
Lydia Gregory <ljg2gb@virginia.edu>
2020-06-26 15:37:20 -0600

    creates User resource and adds controllerr CRUD actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+        render json: @users
+    end
+    
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+    
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+end
