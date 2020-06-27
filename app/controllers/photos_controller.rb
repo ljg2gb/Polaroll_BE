71ddf39dfa2b3f2322f4a93637fee5c7d0967b32
Lydia Gregory <ljg2gb@virginia.edu>
2020-06-27 11:14:21 -0600

    adds working CRUD photo controller actions  with authentication

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,3 +1,30 @@
 class PhotosController < ApplicationController
-    belongs_to :user
+    before_action :authenticate, only: [:index, :create]
+
+    def index
+        @photos = Photo.all
+        render json: @photos
+    end
+
+    def show
+        @photo = Photo.find(params[:id])
+        render json: @photo
+    end
+
+    def create
+        @photo = Photo.create(photo_params)
+        render json: @photo
+    end
+
+    def destroy
+        @photo = Photo.find(params[:id])
+        @photo.destroy
+        render json: {message: "Photo successfull deleted."}
+    end
+
+    private 
+
+    def photo_params
+        params.require(:photo).permit(:link, :notes).merge(user_id: @user_id)
+    end
 end
