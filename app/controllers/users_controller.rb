493040b6e35b55bc19717920c4b14d1538d43f65
Lydia Gregory <ljg2gb@virginia.edu>
2020-07-01 16:08:26 -0600

    refactors user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,24 +18,24 @@ class UsersController < ApplicationController
         # , include: [:friends]
     end
     
-    def create
-        @user = User.create(user_params)
-        render json: @user
-    end
-
     # def create
-    #     @user = User.new(user_params)
-
-    #     if @user.save
-    #         token = create_token(@user)
-    #         render json: {
-    #             message: "Success user #{@user.name} has been created.",
-    #             token: token
-    #         }
-    #     else
-    #         render json: { message: @user.errors.messages }
-    #     end
+    #     @user = User.create(user_params)
+    #     render json: @user
     # end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            token = create_token(@user)
+            render json: {
+                message: "Success user #{@user.name} has been created.",
+                token: token
+            }
+        else
+            render json: { message: @user.errors.messages }
+        end
+    end
 
     private
 
