47028db438c00cea24af1e3e87fab1806914e576
Lydia Gregory <ljg2gb@virginia.edu>
2020-07-05 15:01:37 -0600

    render's identical json object in user login and signup cases

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,25 +3,18 @@ class UsersController < ApplicationController
     before_action :authenticate, only: [:index, :show, :profile]
 
     def profile
-        render json: @user, status: :accepted
+        render json: @user, include: [:photos], status: :accepted
     end
 
     def index
         @users = User.all
         render json: @users
-        # , include: [:friends] 
     end
     
     def show
         @user = User.find(params[:id])
         render json: @user
-        # , include: [:friends]
     end
-    
-    # def create
-    #     @user = User.create(user_params)
-    #     render json: @user
-    # end
 
     def create
         @user = User.new(user_params)
@@ -29,8 +22,9 @@ class UsersController < ApplicationController
         if @user.save
             token = create_token(@user)
             render json: {
-                message: "Success user #{@user.name} has been created.",
-                token: token
+                token: token,
+                user_id: @user.id,
+                user_name: @user.name
             }
         else
             render json: { message: @user.errors.messages }
